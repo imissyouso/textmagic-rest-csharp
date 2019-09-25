125afd885a1b765488bb957083ce406fd751b7f1
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-25 09:55:00 +0000

    Uncomment bauild images and dredd tests

--- src/TextMagicClient/Properties/AssemblyInfo.cs
+++ src/TextMagicClient/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.399")]
-[assembly: AssemblyFileVersion("2.0.399")]
+[assembly: AssemblyVersion("2.0.400")]
+[assembly: AssemblyFileVersion("2.0.400")]
