335ee3ed13025c4a3b1f39dba675b48ed62e82da
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-30 12:29:21 +0000

    Add Python SDK generator

--- src/TextMagicClient/Client/Configuration.cs
+++ src/TextMagicClient/Client/Configuration.cs
@@ -29,7 +29,7 @@ namespace TextMagicClient.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "2.0.424";
+        public const string Version = "2.0.425";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -422,7 +422,7 @@ namespace TextMagicClient.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 2\n";
-            report += "    SDK Package Version: 2.0.424\n";
+            report += "    SDK Package Version: 2.0.425\n";
 
             return report;
         }
