22c01806d3887e8fb396af2c80309ad5c4accd27
Andrey Vorobyev <vorobivka@mail.ru>
2019-10-03 10:00:01 +0000

    Finx conflictsush

--- src/TextMagicClient/Properties/AssemblyInfo.cs
+++ src/TextMagicClient/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.455")]
-[assembly: AssemblyFileVersion("2.0.455")]
+[assembly: AssemblyVersion("2.0.456")]
+[assembly: AssemblyFileVersion("2.0.456")]
