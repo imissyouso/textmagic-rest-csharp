bfeeb092ad2b988c6bce8b8b2a6860488e486ed5
Andrey Vorobyev <vorobivka@mail.ru>
2019-10-09 12:02:33 +0000

    Change images format to webp

--- src/TextMagicClient/Properties/AssemblyInfo.cs
+++ src/TextMagicClient/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.582")]
-[assembly: AssemblyFileVersion("2.0.582")]
+[assembly: AssemblyVersion("2.0.583")]
+[assembly: AssemblyFileVersion("2.0.583")]
